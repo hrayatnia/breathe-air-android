8a9cd3bcaeac7bf514cdce350f4166ccc587a3f0
Ali Kabiri <ali@kabiri.org>
2020-01-17 21:58:18 +0100

    add retrofit; add StationData data class as a model; created the post request body model; added the service method to fetch the data

---
+++ app/src/main/java/com/yellowdev/breatheair/model/jsonmodels/GetWeatherDataPostRequestBody.kt
@@ -0,0 +1,10 @@
+package com.yellowdev.breatheair.model.jsonmodels
+
+/**
+ * Created by Ali Kabiri on 2020-01-17.
+ */
+
+data class GetWeatherDataPostRequestBody (
+    var lat: String,
+    var long: String
+)
--- app/src/main/java/com/yellowdev/breatheair/network/BreatheAirService.kt
+++ app/src/main/java/com/yellowdev/breatheair/network/BreatheAirService.kt
@@ -1,10 +1,19 @@
 package com.yellowdev.breatheair.network
 
+import com.yellowdev.breatheair.model.jsonmodels.StationData
+import com.yellowdev.breatheair.model.jsonmodels.GetWeatherDataPostRequestBody
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 /**
  * Created by Ali Kabiri on 2020-01-05.
  */
 
 interface BreatheAirService {
+
+    @POST("/location")
+    suspend fun getData(@Body requestBody: GetWeatherDataPostRequestBody): Response<StationData>
 
     companion object {
         const val BASE_URL = "https://breatherair.herokuapp.com"
