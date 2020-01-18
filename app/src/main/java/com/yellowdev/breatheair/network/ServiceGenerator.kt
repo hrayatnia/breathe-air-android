d067bb98f6910ecea08cdaa3511f5e17ec8ccf59
Ali Kabiri <ali@kabiri.org>
2020-01-18 01:21:57 +0100

    added the service generator class

--- app/src/main/java/com/yellowdev/breatheair/network/ServiceGenerator.kt
+++ app/src/main/java/com/yellowdev/breatheair/network/ServiceGenerator.kt
@@ -1,8 +1,29 @@
 package com.yellowdev.breatheair.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 /**
  * Created by Ali Kabiri on 2020-01-05.
  */
 
 class ServiceGenerator {
+
+    companion object {
+
+        fun getBreatheAirService(): BreatheAirService {
+
+            val builder = Retrofit.Builder()
+                .baseUrl(BreatheAirService.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+
+            val retrofit = builder.build()
+
+            // define interceptors.
+            // define custom okhttp client.
+
+            // add the interceptor and custom okhttp client.
+            return retrofit.create(BreatheAirService::class.java)
+        }
+    }
 }
