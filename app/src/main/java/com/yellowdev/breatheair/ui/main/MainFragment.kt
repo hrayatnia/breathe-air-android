2401ad95009528a2d49afe1ff54afe56af8e7446
Ali Kabiri <ali@kabiri.org>
2020-02-17 23:05:16 +0100

    updated MainFragment to use the ViewModel to fetch the data for testing; updated the README.md file

--- app/src/main/java/com/yellowdev/breatheair/ui/main/MainFragment.kt
+++ app/src/main/java/com/yellowdev/breatheair/ui/main/MainFragment.kt
@@ -1,16 +1,19 @@
 package com.yellowdev.breatheair.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.yellowdev.breatheair.R
 
 class MainFragment : Fragment() {
 
     companion object {
+        private const val TAG = "MainActivity"
         fun newInstance() = MainFragment()
     }
 
@@ -23,8 +26,15 @@ class MainFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
+        // lat, lang of the Kölle!
+        val lat = 50.935173
+        val lang = 6.953101
+        // TODO - replace this after the UI is ready.
+        val stationData = viewModel.getStationData(lat.toString(), lang.toString())
+        stationData.observe(viewLifecycleOwner, Observer {
+            Log.d(TAG, it.location.name)
+        })
+    }
 }
